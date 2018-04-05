da22afd38df01ec0446abba96544dfb0f1304c4b
Spyda <cryptobyte1134@gmail.com>
2018-04-04 23:16:12 -0400

    Update to ECMAScript 6

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,5 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 // set up a mongoose model
 module.exports = mongoose.model('User', new Schema({ 
